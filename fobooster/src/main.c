7c0e1725876129a112caa5c504f65026a1ca73a8
Sean Cross <sean@xobs.io>
2019-06-17 11:13:14 -0700

    fobootster: first working version
    This now properly builds and runs on real hardware.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- fobooster/src/main.c
+++ fobooster/src/main.c
@@ -1,15 +1,29 @@
 #include <fobooster.h>
 #include <csr.h>
 #include <spi.h>
+#include <rgb.h>
 
 // Defined in the linker
 extern struct fobooster_data fobooster_data;
 extern const struct fobooster_data fobooster_src;
 
+void msleep(int ms)
+{
+    timer0_en_write(0);
+    timer0_reload_write(0);
+    timer0_load_write(SYSTEM_CLOCK_FREQUENCY/1000*ms);
+    timer0_en_write(1);
+    timer0_update_value_write(1);
+    while(timer0_value_read())
+        timer0_update_value_write(1);
+}
+
 __attribute__((noreturn)) void reboot(void)
 {
     uint8_t image_index = 0;
     reboot_ctrl_write(0xac | (image_index & 3) << 0);
+    while (1)
+        ;
     __builtin_unreachable();
 }
 
@@ -33,34 +47,13 @@ static void ftfl_begin_erase_sector(uint32_t address)
     spiBeginErase4(address);
 }
 
-uint32_t bytes_left;
-uint32_t target_addr;
-uint32_t *current_ptr;
-uint32_t page_offset;
-
 // Erase the Toboot configuration, which will prevent us from
 // overwriting Toboot again.
 // Run this from RAM to ensure we don't hardfault.
 // Reboot after we're done.
 __attribute__((noreturn)) static void finish_flashing(void)
 {
-#if 0
-    uint32_t vector_addr = (uint32_t)&toboot_configuration;
-
-    // Jump back into the bootloader when we reboot
-    toboot_runtime.magic = TOBOOT_FORCE_ENTRY_MAGIC;
-
-    while (MSC->STATUS & MSC_STATUS_BUSY)
-        ;
-
-    MSC->WRITECTRL |= MSC_WRITECTRL_WREN;
-    MSC->ADDRB = vector_addr;
-    MSC->WRITECMD = MSC_WRITECMD_LADDRIM;
-    MSC->WRITECMD = MSC_WRITECMD_ERASEPAGE;
-
-    while (MSC->STATUS & MSC_STATUS_BUSY)
-        ;
-#endif
+    ftfl_busy_wait();
     reboot();
 }
 
@@ -71,19 +64,34 @@ void isr(void)
 
 __attribute__((noreturn)) void fobooster_main(void)
 {
-    // If the booster data is too big, just let the watchdog timer reboot us,
-    // since the program is invalid.
-    if (fobooster_src.payload_size != 104090)
+    uint32_t bytes_left;
+    uint32_t target_addr;
+    void *current_ptr;
+    uint32_t page_offset;
+
+    rgb_init();
+
+    // If the booster data is not correct, the program is invalid.
+    if (fobooster_src.payload_size != 104250)
     {
+        rgb_mode_error();
+        msleep(1000);
         reboot();
     }
 
     // We want to run entirely from RAM, so copy the booster payload to RAM too.
-    memcpy((void *restrict) & fobooster_src, &fobooster_data, fobooster_src.payload_size);
+    memcpy((void *restrict) &fobooster_data, &fobooster_src, sizeof(struct fobooster_data));
+    memcpy((void *restrict) &fobooster_data.payload[0], &fobooster_src.payload[0], fobooster_data.payload_size);
 
     // Ensure the hash matches what's expected.
     if (XXH32(fobooster_data.payload, fobooster_data.payload_size, FOBOOSTER_SEED) != fobooster_data.xxhash)
     {
+        rgb_mode_error();
+        msleep(1000);
+        msleep(1000);
+        msleep(1000);
+        msleep(1000);
+        msleep(1000);
         reboot();
     }
 
@@ -100,19 +108,21 @@ __attribute__((noreturn)) void fobooster_main(void)
         ftfl_begin_erase_sector(target_addr);
 
         for (page_offset = 0;
-             bytes_left && page_offset < SPI_ERASE_SECTOR_SIZE;
+             bytes_left && (page_offset < SPI_ERASE_SECTOR_SIZE);
              page_offset += SPI_PROGRAM_PAGE_SIZE)
         {
             uint32_t bytes_to_write = bytes_left;
             if (bytes_to_write > SPI_PROGRAM_PAGE_SIZE)
                 bytes_to_write = SPI_PROGRAM_PAGE_SIZE;
-            spiBeginWrite(target_addr, current_ptr, bytes_to_write);
             ftfl_busy_wait();
+            spiBeginWrite(target_addr, current_ptr, bytes_to_write);
             current_ptr += SPI_PROGRAM_PAGE_SIZE;
             target_addr += SPI_PROGRAM_PAGE_SIZE;
             bytes_left -= bytes_to_write;
         }
     }
 
+    rgb_mode_writing();
+    msleep(1000);
     finish_flashing();
 }
