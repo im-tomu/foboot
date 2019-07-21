a363e25b76743b0dd26c4e0e7c799454cdca9e1e
Sean Cross <sean@xobs.io>
2019-07-21 12:29:20 +0800

    sw: time: rename SYSTEM_CLOCK_FREQUENCY to CONFIG_CLOCK_FREQUENCY
    This changed in litex.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- sw/third_party/libbase/time.c
+++ sw/third_party/libbase/time.c
@@ -6,7 +6,7 @@ void time_init(void)
 	int t;
 
 	timer0_en_write(0);
-	t = 2*SYSTEM_CLOCK_FREQUENCY;
+	t = 2*CONFIG_CLOCK_FREQUENCY;
 	timer0_reload_write(t);
 	timer0_load_write(t);
 	timer0_en_write(1);
@@ -36,7 +36,7 @@ void msleep(int ms)
 {
         timer0_en_write(0);
         timer0_reload_write(0);
-        timer0_load_write(SYSTEM_CLOCK_FREQUENCY/1000*ms);
+        timer0_load_write(CONFIG_CLOCK_FREQUENCY/1000*ms);
         timer0_en_write(1);
         timer0_update_value_write(1);
         while(timer0_value_read()) timer0_update_value_write(1);
