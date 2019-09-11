cf8273d8c1ad903f5c2c19e267816424bb931bff
Sean Cross <sean@xobs.io>
2019-09-11 13:29:03 +0800

    sw: usb: add set_address support
    Signed-off-by: Sean Cross <sean@xobs.io>

--- sw/include/usb.h
+++ sw/include/usb.h
@@ -1,6 +1,8 @@
 #ifndef __USB_H
 #define __USB_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -18,10 +20,11 @@ void usb_setup(const struct usb_setup_request *setup);
 void usb_send(const void *data, int total_count);
 void usb_ack_in(void);
 void usb_ack_out(void);
-void usb_err(void);
+void usb_err(uint8_t ep);
 int usb_recv(void *buffer, unsigned int buffer_len);
 void usb_poll(void);
 void usb_wait_for_send_done(void);
+void usb_set_address(uint8_t new_address);
 
 #ifdef __cplusplus
 }
