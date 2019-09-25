1babc9c2c80c12032a4e0cf4b36c172ce20a4b33
Sean Cross <sean@xobs.io>
2019-09-25 12:57:06 +0800

    hw: add a dependency on nextpnr-ice40
    This used to be implicit in other dependencies.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- hw/foboot-bitstream.py
+++ hw/foboot-bitstream.py
@@ -3,7 +3,7 @@
 # relies on.  lxbuildenv reads this variable in order to ensure
 # the build will finish without exiting due to missing third-party
 # programs.
-LX_DEPENDENCIES = ["riscv", "icestorm", "yosys"]
+LX_DEPENDENCIES = ["riscv", "icestorm", "yosys", "nextpnr-ice40"]
 
 # Import lxbuildenv to integrate the deps/ directory
 import lxbuildenv
