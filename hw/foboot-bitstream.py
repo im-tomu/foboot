1f4401fc5182952a58edb41099a67afd147bbcc2
Sean Cross <sean@xobs.io>
2021-11-25 09:41:07 +0800

    Revert "bitstream: use 32-bit csrs"
    While 32-bit CSRs increase performance by 4x, they cause older software
    to break.
    
    Consider making this breaking change in 3.0.
    
    This reverts commit bb0b1ff761cc86421ed1838986ad179b0124f794.

--- hw/foboot-bitstream.py
+++ hw/foboot-bitstream.py
@@ -235,7 +235,7 @@ class BaseSoC(SoCCore, AutoDoc):
         clk_freq = int(12e6)
         self.submodules.crg = _CRG(platform)
 
-        SoCCore.__init__(self, platform, clk_freq, integrated_sram_size=0, with_uart=False, csr_data_width=32, **kwargs)
+        SoCCore.__init__(self, platform, clk_freq, integrated_sram_size=0, with_uart=False, **kwargs)
 
         usb_debug = False
         if debug is not None:
